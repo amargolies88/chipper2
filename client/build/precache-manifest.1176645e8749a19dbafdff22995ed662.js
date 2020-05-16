7414a6073fdd11e4b009bd54db793b4c4fe6063c
Alan Margolies <amargolies88@gmail.com>
2020-05-16 02:23:29 -0400

    i love chipper

---
+++ client/build/precache-manifest.1176645e8749a19dbafdff22995ed662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1576bb4b3cee325b7353d51c1156a22a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744bf461ff4229a4e625",
+    "url": "/static/css/main.3fb6925e.chunk.css"
+  },
+  {
+    "revision": "cff730192effbb357518",
+    "url": "/static/js/2.eecfac8f.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.eecfac8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744bf461ff4229a4e625",
+    "url": "/static/js/main.63768d83.chunk.js"
+  },
+  {
+    "revision": "9ac7cb649d0b498cd997",
+    "url": "/static/js/runtime-main.103590a6.js"
+  },
+  {
+    "revision": "5e4eb31914c7a6c502967517fab7b79f",
+    "url": "/static/media/happydog.5e4eb319.jpeg"
+  }
+]);
